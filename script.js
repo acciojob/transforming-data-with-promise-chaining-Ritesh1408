fb719066f48a10afd5e3ce670e091effbddc09e6
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-03-16 20:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-//your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function() {
+    let input = parseFloat(document.getElementById("ip").value);
+    let outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = ""; 
+    
+    new Promise((resolve) => {
+        setTimeout(() => {
+            outputDiv.innerHTML += `Result: ${input} <br>`;
+            resolve(input);
+        }, 2000);
+    })
+    .then((num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let result = num * 2;
+                outputDiv.innerHTML += `Result: ${result} <br>`;
+                resolve(result);
+            }, 2000);
+        });
+    })
+    .then((num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let result = num - 3;
+                outputDiv.innerHTML += `Result: ${result} <br>`;
+                resolve(result);
+            }, 1000);
+        });
+    })
+    .then((num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let result = num / 2;
+                outputDiv.innerHTML += `Result: ${result} <br>`;
+                resolve(result);
+            }, 1000);
+        });
+    })
+    .then((num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let result = num + 10;
+                outputDiv.innerHTML += `Final Result: ${result} <br>`;
+                resolve(result);
+            }, 1000);
+        });
+    });
+});
